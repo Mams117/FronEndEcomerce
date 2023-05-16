4462e236883d178696be32f77e3b152ff7bcd844
Mario Miranda <adsoadso17@gmail.com>
2023-05-15 20:30:01 -0500

    interfaz usuario

---
+++ Front/main.js
@@ -0,0 +1,10 @@
+const url_Api="http://localhost:4000/productos";
+
+fetch(url_Api)
+.then((response)=>response.json())
+.then((res)=>{
+    console.log(res)
+    for (let index = 0; index < res.length; index++) {
+        document.write(res[index].nombre)
+    }
+})
